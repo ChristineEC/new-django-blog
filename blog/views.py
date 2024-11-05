a9d4f5ec7b9580edaabfa1b54184c94963c050c3
Christine Carter <pxe50zgd@students.codeinstitute.net>
2024-11-05 20:20:22 +0000

    Remove print statements

--- blog/views.py
+++ blog/views.py
@@ -38,7 +38,6 @@ def post_detail(request, slug):
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
     if request.method == "POST":
-        print("Received a POST request")
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
             comment = comment_form.save(commit=False)
@@ -51,8 +50,6 @@ def post_detail(request, slug):
             )
 
     comment_form = CommentForm()
-
-    print("About to render the template")
 
     return render(
         request,
